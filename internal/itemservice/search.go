a5172f1d58c328a39e87070a54b76e2257a4a1b7
Philipp Böschen <boeschenphilipp@gmail.com>
2021-02-22 13:02:06 +0100

    remove empty results add linksearch func

--- internal/itemservice/search.go
+++ internal/itemservice/search.go
@@ -2,6 +2,7 @@ package itemservice
 
 import (
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/Deichindianer/poe-item-bot/internal/characterpoller"
@@ -11,26 +12,42 @@ import (
 func (i *ItemService) search(c *gin.Context) {
 	typeSearchString := c.Query("type")
 	modSearchString := c.Query("mod")
+	// linkSearchString := c.Query("links")
 	result := SearchResult{}
 	characterList := i.characterPoller.GetCharacters()
 
 	if typeSearchString != "" {
 		for _, cw := range characterList {
-			var match Match
-			match.AccountName = cw.AccountName
 			tsr := typeSearch(typeSearchString, cw.Items)
-			match.Items = tsr
-			result.Result = append(result.Result, match)
+			if tsr != nil {
+				var match Match
+				match.AccountName = cw.AccountName
+				match.Items = tsr
+				result.Result = append(result.Result, match)
+			}
 		}
-	} else {
+	} else if modSearchString != "" {
 		for _, cw := range characterList {
-			var match Match
-			match.AccountName = cw.AccountName
 			msr := modSearch(modSearchString, cw.Items)
-			match.Items = msr
-			result.Result = append(result.Result, match)
+			if msr != nil {
+				var match Match
+				match.AccountName = cw.AccountName
+				match.Items = msr
+				result.Result = append(result.Result, match)
+			}
 		}
 	}
+	// else if linkSearchString != "" {
+	// 	for _, cw := range characterList {
+	// 		lsr := modSearch(modSearchString, cw.Items)
+	// 		if lsr != nil {
+	// 			var match Match
+	// 			match.AccountName = cw.AccountName
+	// 			match.Items = lsr
+	// 			result.Result = append(result.Result, match)
+	// 		}
+	// 	}
+	// }
 	c.JSON(http.StatusOK, result)
 	return
 }
@@ -50,6 +67,18 @@ func modSearch(search string, items []characterpoller.Item) []characterpoller.It
 	for _, item := range items {
 		for _, mod := range item.ExplicitMods {
 			if strings.Contains(strings.ToLower(mod), strings.ToLower(search)) {
+				result = append(result, item)
+			}
+		}
+	}
+	return result
+}
+
+func linkSearch(search string, items []characterpoller.Item) []characterpoller.Item {
+	var result []characterpoller.Item
+	for _, item := range items {
+		for _, link := range item.Sockets {
+			if strconv.Itoa(link.GroupID) == search {
 				result = append(result, item)
 			}
 		}
